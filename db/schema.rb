b51e7427e22bc952e2c760149370ad9144bff5b4
Keegan Noble <keegannoble90@gmail.com>
2020-08-27 15:29:09 +0800

    created scaffolds for List and Card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_035341) do
+ActiveRecord::Schema.define(version: 2020_08_27_072151) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "list_id", null: false
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_08_27_035341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "lists"
 end
