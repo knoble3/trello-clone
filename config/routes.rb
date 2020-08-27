31cda318f8b0436c4c1c0677daa98ef82f3dbe8d
Keegan Noble <keegannoble90@gmail.com>
2020-08-27 15:35:36 +0800

    set routes and added act_as_list gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cards
   resources :lists
-  root to: 'pages#home'
+  root to: 'lists#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
